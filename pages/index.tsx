ac727e9977a3d38f718d99f5dd3ccbf4ecb5076f
jiheondev3 <jiheondev3@gmail.com>
2022-11-24 09:25:00 -0800

    Add hardcoded list of fancams

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,26 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+type Fancam = {
+  song: {
+    name: string,
+  },
+  quality: string,
+  url: string,
+  title: string,
+}
+
+const Item = ({fancam}: {fancam: Fancam}) => {
+  return (
+    <ul>
+      <li dangerouslySetInnerHTML={{__html:`
+      ${fancam.song.name} | ${fancam.quality}: <a href="${fancam.url}" target="_blank">${fancam.title}</a>  <!-- <iframe width="800em" height="100em" width="${200/315*560}em" height="${200}em" src="https://www.youtube.com/embed/KUcScWwYdqg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+      `}}>
+      </li>
+    </ul>
+  )
+}
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,59 +33,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to 직캠!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Item fancam={{song:{name:'DKDK'}, quality:'1080p60', url: 'https://youtu.be/KUcScWwYdqg?t=11', title: '[60f] 180916 프로미스나인(fromis_9) 백지헌(Jiheon)- 두근두근(DKDK) @안산 희망마라톤 직캠(fancam) by 땀맨(SweatMan)'}} />
+        <Item fancam={{song:{name:'DKDK'}, quality:'1080p50', url: 'https://www.youtube.com/watch?v=Mw-qECpkTF4', title: `프로미스나인 백지헌 직캠 - 두근두근 (Fromis_9 - DKDK ' Baek Ji Heon' Focus CAM)`}} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
